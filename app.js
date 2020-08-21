81185aa3ab98ef59ed923c4103c37f15654eac2f
SYONG <ghdgus24@naver.com>
2020-08-21 14:43:55 +0900

    라우팅 완료

--- app.js
+++ app.js
@@ -3,25 +3,21 @@ import morgan from "morgan"; // loging 관련 미들웨어
 import helmet from "helmet"; // nodeJS 보안 관련 미들웨어
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
 
 const app = express(); // app 이라는 변수에 express를 실행해서 담은 것.
 
-const handleHome = (req, res) => {
-	res.send("hi from home"); // 뭔가 응답하려면 이렇게 send
-};
-const handleProfile = (req, res) => {
-	res.send("you are on my profile");
-};
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser()); // cookie를 전달받아서 사용할 수 있도록 만들어주는 미들웨어
+app.use(bodyParser.json()); // 사용자가 웹으로 전달하는 데이터들을 검사하는 미들웨어
+app.use(bodyParser.urlencoded({ extended: true })); // form 혹은 json 형태로 된 body를 검사한다.
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome); // 보통 get, post 시에 실행하는 함수는 항상 request객체, response객체 인자를 필요로함
-app.get("/profile", handleProfile);
-app.use("/user", userRouter); // use를 쓴 이유는, userRouter 전체를 사용하겠다는 의미
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
